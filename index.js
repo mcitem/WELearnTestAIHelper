f3372636e0dd4e4de8e9864f2694da46f974e5ab
mcitem <mcitem@mcitem.com>
2024-05-13 23:31:29 +0800

    适配welearn子域名测试页

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WELearn Test AI Helper ; 使用讯飞语音听写配合AI获得welearn听力测试的答案
 // @namespace    https://github.com/mcitem
-// @version      1.1
+// @version      1.1.1
 // @description  使用讯飞语音听写配合AI获得welearn听力测试的答案; 这是一个用于获取题目数据的脚本，需要配合项目中的python使用，项目地址https://github.com/mcitem/WELearnTestAIHelper
 // @author       MCitem
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wetest.sflep.com
@@ -51,8 +51,6 @@
 
     // 等待
     await sleep(4000)
-    const part1 = document.getElementById('part1')
-    const itemDivs = part1.getElementsByClassName('itemDiv')
 
     function createData_itemDiv(link='',hov=[]) {
         return {
@@ -66,7 +64,72 @@
             'choices':choises
         }
     }
+    console.log( window.location.hostname)
 
+    if (window.location.hostname == 'wetest.sflep.com') {
+        wetestMain()
+    }
+    if (window.location.hostname == 'welearn.sflep.com') {
+        welearnMain()
+    }
+
+    function welearnMain(){
+        const activePart  = document.querySelector('div.active')
+        const itemDivs = activePart.querySelectorAll('.itemDiv')
+        let data_itemDivs = []
+        let all_links = []
+        let count = 1; //题目序号
+        for (let i = 0; i < itemDivs.length; i++) {
+            let test_center = itemDivs[i].querySelector('.text-center')
+            let test_center_link = str(test_center.querySelector('a').href)
+            console.log(i+1,test_center_link);
+
+            all_links.push(test_center_link)
+            let data_itemDiv = createData_itemDiv(test_center_link)
+    
+            let test_hovs = itemDivs[i].getElementsByClassName('test_hov')
+    
+            for (let j = 0; j < test_hovs.length; j++) {
+                let test_hov_link =  str(test_hovs[j].getElementsByTagName('a')[0].href)
+                console.log('count',count,test_hov_link);
+    
+                var data_itemDiv_test_hov = createData_itemDiv_test_hov(test_hov_link)
+                all_links.push(test_hov_link)
+    
+                let choicelist = test_hovs[j].getElementsByClassName('choiceList')[0]
+      
+    
+                let choices = choicelist.getElementsByTagName('label')
+    
+                let choicount = 1; //选项序号（不太需要
+                for (let k = 0; k < choices.length; k++) {
+                    let choi = choices[k].innerText
+                    console.log(count,choicount,choi);
+                    choicount = choicount + 1;
+                    data_itemDiv_test_hov.choices.push(choi)
+                }
+                count = count + 1;
+                data_itemDiv.test_hovs.push(data_itemDiv_test_hov)
+            }
+            data_itemDivs.push(data_itemDiv)
+            
+        }
+        console.log(data_itemDivs);
+        
+        let copy = {
+            "all_links":all_links,
+            "data":data_itemDivs
+        }
+        textarea.textContent = JSON.stringify(copy,null,2)
+        navigator.clipboard.writeText(JSON.stringify(copy,null,2)).then(function() {
+            console.log('Copying successful!');
+        })
+
+    }
+
+    function wetestMain(){
+    const part1 = document.getElementById('part1')
+    const itemDivs = part1.getElementsByClassName('itemDiv')
     let data_itemDivs = []
     let all_links = []
     let count = 1; //题目序号
@@ -113,7 +176,7 @@
     }
     textarea.textContent = JSON.stringify(copy,null,2)
     navigator.clipboard.writeText(JSON.stringify(copy,null,2)).then(function() {
-        console.log('Async: Copying to clipboard was successful!');
+        console.log('Copying successful!');
     })
-
+}
 })();
